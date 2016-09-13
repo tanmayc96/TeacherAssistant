7c8f6532190ba1d49c4a9c0ef51014b86348b9f9
Tanmay Chanda <Tanmay Chanda>
2016-09-14 01:02:21 +0530

    14/9

---
+++ Edit2.java
@@ -0,0 +1,13 @@
+package com.example.dellpctc.teacherassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Edit2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit2);
+    }
+}
